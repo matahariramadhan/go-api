b10d67f48e84bb0415b80dedf2bf25090af92843
Matahari Ramadhan <ramadhan.matahari24@gmail.com>
2022-10-22 19:25:44 +0200

    creating createBooks handler

--- main.go
+++ main.go
@@ -10,22 +10,35 @@ type book struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
 	Author   string `json:"author"`
-	Quantity string `json:"quantity"`
+	Quantity int    `json:"quantity"`
 }
 
 var books = []book{
-	{ID: "1", Title: "Buku pertama", Author: "Author pertama", Quantity: "1"},
-	{ID: "2", Title: "Buku kedua", Author: "Author kedua", Quantity: "2"},
-	{ID: "3", Title: "Buku ketiga", Author: "Author ketiga", Quantity: "3"},
+	{ID: "1", Title: "Buku pertama", Author: "Author pertama", Quantity: 1},
+	{ID: "2", Title: "Buku kedua", Author: "Author kedua", Quantity: 2},
+	{ID: "3", Title: "Buku ketiga", Author: "Author ketiga", Quantity: 3},
 }
 
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
+
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
 
 	router.Run("localhost:8080")
 }
